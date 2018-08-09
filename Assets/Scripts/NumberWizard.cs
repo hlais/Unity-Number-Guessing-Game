18eb12911e13915964ad3131f4dfecccf3ca3c27
Halim Lais <halimlais@hotmail.com>
2018-08-09 16:24:28 +0100

    Initial commit

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class NumberWizard : MonoBehaviour {
+
+    [SerializeField] int max;
+    [SerializeField]int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+   
+
+    // Use this for initialization
+    void Start ()
+    {
+        StartGame();
+       
+        
+    }
+	
+    void StartGame()
+    {
+
+        NextGuess();
+       
+        
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+         
+
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess + 1 ;
+        NextGuess();
+    }
+    public void OnPressLower()
+    {
+        max = guess -1;
+        NextGuess();
+    }
+    void NextGuess()
+    {
+        guess = Random.Range(min,max+1);
+        //casting intGuess To string
+        guessText.text = guess.ToString();
+
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//added name space
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
